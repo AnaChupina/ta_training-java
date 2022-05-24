997ccb8db2a2435bfa25a9b98c6f8f5e3bf6550d
Anastasia Chupina <an.v.chupina@gmail.com>
2022-05-24 22:02:54 +0400

    First programes in Java

---
+++ Java/src/com/epam/training/student_anastasia_chupina/first/HelloAutocode.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_anastasia_chupina.first;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+
+    }
+}

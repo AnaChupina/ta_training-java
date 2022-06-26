23234ebe5e6efe660a2933c9ebc5e909d9eebb2e
Anastasia Chupina <an.v.chupina@gmail.com>
2022-06-26 23:00:47 +0400

    Decrementing Carousel

---
+++ Java/src/com/epam/training/student_anastasia_chupina/fifth/carousel/DecrementingCarousel.java
@@ -0,0 +1,32 @@
+package com.epam.training.student_anastasia_chupina.fifth.carousel;
+
+public class DecrementingCarousel {
+    int [] carousel;
+    public int capacity;
+    int i = 0;
+    int runNumber = 0;
+
+    public DecrementingCarousel(int capacity) {
+        this.capacity = capacity;
+        carousel = new int[capacity];
+    }
+
+    public boolean addElement(int element){
+        if (runNumber == 0 && i<capacity && element > 0 ) {
+            carousel [i] = element;
+            i++; // i is index of an element
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public CarouselRun run(){
+        if (runNumber == 0){
+            runNumber = 1;
+            return new CarouselRun(carousel);
+        } else {
+            return null;
+        }
+    }
+}

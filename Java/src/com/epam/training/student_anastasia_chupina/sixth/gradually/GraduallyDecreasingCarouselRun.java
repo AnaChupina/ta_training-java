9b4f28d0cde183d18bbf6f441269922aac9bd06b
Anastasia Chupina <an.v.chupina@gmail.com>
2022-08-10 23:16:52 +0400

    Gradually Decreasing Carousel

---
+++ Java/src/com/epam/training/student_anastasia_chupina/sixth/gradually/GraduallyDecreasingCarouselRun.java
@@ -0,0 +1,32 @@
+package com.epam.training.student_anastasia_chupina.sixth.gradually;
+
+public class GraduallyDecreasingCarouselRun extends CarouselRun{
+    int k = 0;
+
+    public GraduallyDecreasingCarouselRun(int[] carousel) {
+        super(carousel);
+    }
+    public int next() {
+        if (isFinished()){
+            return -1;
+        }
+        for (; ; j=(j+1)%carousel.length) {
+            if (j == 0){
+                k++;
+            }
+            if (carousel[j] != 0) {
+                nextReturn = carousel[j];
+                if (k > carousel[j]) {
+                    carousel[j] = 0;
+                    j = (j+1)%carousel.length;
+                    return nextReturn;
+                } else {
+                    carousel[j] = carousel[j]-k;
+                    j = (j+1)%carousel.length;
+                    return nextReturn;
+                }
+
+            }
+        }
+    }
+}

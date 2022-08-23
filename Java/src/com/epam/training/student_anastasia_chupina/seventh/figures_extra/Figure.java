d165a57ae7766027709748382747965779bea368
Anastasia Chupina <an.v.chupina@gmail.com>
2022-08-23 21:26:49 +0400

    Figures.Extra Challenge

---
+++ Java/src/com/epam/training/student_anastasia_chupina/seventh/figures_extra/Figure.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_anastasia_chupina.seventh.figures_extra;
+
+abstract class Figure{
+
+    public abstract Point centroid();
+    public abstract boolean isTheSame(Figure figure);
+}

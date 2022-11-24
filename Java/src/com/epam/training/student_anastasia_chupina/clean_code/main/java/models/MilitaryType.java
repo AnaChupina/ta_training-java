b0fffe5727c888caa8e8a6560aff50a057462479
Anastasia Chupina <an.v.chupina@gmail.com>
2022-11-24 19:54:03 +0400

    Clean code

---
+++ Java/src/com/epam/training/student_anastasia_chupina/clean_code/main/java/models/MilitaryType.java
@@ -0,0 +1,5 @@
+package com.epam.training.student_anastasia_chupina.clean_code.main.java.models;
+
+public enum MilitaryType {
+    FIGHTER, BOMBER, TRANSPORT
+}
---
+++ Java/src/com/epam/training/student_anastasia_chupina/clean_code/main/java/planes/MyPlane.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_anastasia_chupina.clean_code.main.java.planes;
+
+public class MyPlane  extends Plane {
+    public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+    }
+}
